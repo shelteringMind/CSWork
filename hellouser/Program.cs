d54c47ce83e21ed0af553a6cb8948548eb30f274
Anton Kruglyakov <akrugs@mail.ru>
2023-03-05 20:59:37 +0500

    исправили условие для проверки всех возможных комбинаций ввода имени

--- hellouser/Program.cs
+++ hellouser/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if(username == "Антон") {
+if(username.ToLower()  == "антон") {
     Console.WriteLine("Ба, какие люди! Привет, "+username+"!");
 }
 else {
