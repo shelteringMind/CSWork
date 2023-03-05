2151cdfcd7250c71fc7a7e9fd0fc4dd9ef453cca
Anton Kruglyakov <akrugs@mail.ru>
2023-03-05 20:46:26 +0500

    доавили проверку имени на соответствие заданному

--- hellouser/Program.cs
+++ hellouser/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-Console.Write("Здравствуйте, "+username);
+if(username == "Антон") {
+    Console.WriteLine("Ба, какие люди! Привет, "+username+"!");
+}
+else {
+    Console.Write("Здравствуйте, "+username+"!");
+}
