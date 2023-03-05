67800691cb6b0d821810bf38ade11d608e3cd4e2
Anton Kruglyakov <akrugs@mail.ru>
2023-03-05 21:21:26 +0500

    создали проект поиска максимального веса

---
+++ Base/Program.cs
@@ -0,0 +1,20 @@
+﻿int weight1=5;
+int weight2=3;
+int weight3=14;
+int weight4=9;
+int weight5=1;
+int maxWeight=weight1;
+if(weight2>maxWeight){
+    maxWeight=weight2;
+}
+if(weight3>maxWeight) {
+    maxWeight=weight3;
+}
+if(weight4>maxWeight) {
+    maxWeight=weight4;
+}
+if(weight5>maxWeight) {
+    maxWeight=weight5;
+}
+Console.WriteLine("Самая большая гиря весит: " + maxWeight);
+
