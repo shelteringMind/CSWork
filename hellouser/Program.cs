d2bafd2af77452e44a7f9ff81abba7bc9c9ec28b
Anton Kruglyakov <akrugs@mail.ru>
2023-03-05 19:21:31 +0500

    Создали проект запроса имени пользователя

---
+++ hellouser/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+Console.Write("Здравствуйте, "+username);
