78af2b944a4c3c2be5e9cec3f655da5ff876a332
Anton Kruglyakov <akrugs@mail.ru>
2023-03-05 20:07:46 +0500

    add random

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int numberA=0;
-int numberB=0;
+﻿int numberA=new Random().Next(1,101);// [1,100]
+int numberB=new Random().Next(1,101);// [1,100]
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 int resultAB = numberA + numberB;
 Console.WriteLine(resultAB);
-
